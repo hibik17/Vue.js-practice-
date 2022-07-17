7a0caa91be9ca60828dc3e3dfd2c93e33d568388
hibik17 <hbtatsu@gmail.com>
2022-07-17 17:31:55 +0900

    add delete function

--- javascript/main.js
+++ javascript/main.js
@@ -8,11 +8,15 @@ const app = Vue.createApp({
       if (this.newItem === "") return;
       let todo = {
         item: this.newItem,
+        isDone: false,
       };
       this.todos.push(todo);
 
       // タスクの入力完了後に入力欄を空にする動作の追加
       this.newItem = "";
+    },
+    deleteItem: function (index) {
+      this.todos.splice(index, 1);
     },
   },
 });
