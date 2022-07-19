3b286688321b887bfd380b795e05d1309b708759
hibik17 <hbtatsu@gmail.com>
2022-07-19 16:54:44 +0900

    learn v-cloak

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,13 @@
 const app = Vue.createApp({
   data: () => ({
-    newItem: "",
-    todos: [],
+    message: "hello <span style='color: red;'>vue.js!</span>",
   }),
-  methods: {
-    addItem: function (event) {
-      if (this.newItem === "") return;
-      let todo = {
-        item: this.newItem,
-        isDone: false,
-      };
-      this.todos.push(todo);
 
-      // タスクの入力完了後に入力欄を空にする動作の追加
-      this.newItem = "";
-    },
-    deleteItem: function (index) {
-      this.todos.splice(index, 1);
+  methods: {
+    buttonClickAction: function () {
+      this.message = this.message.split("").reverse().join(" ");
     },
   },
 });
+
 app.mount("#app");
