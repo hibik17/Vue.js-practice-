8cd31e193cada525b56bc6eebd87ded9c23d42ff
hibik17 <hbtatsu@gmail.com>
2022-07-20 12:21:18 +0900

    learn cache

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
-  data: () => ({
-    basePrice: 100,
-  }),
+  data: () => ({}),
   computed: {
-    taxIncludedPrice: {
-      get: function () {
-        return this.basePrice * 1.1;
-      },
-      set: function (value) {
-        this.basePrice = value / 1.1;
-      },
+    computedNumber: function () {
+      console.log("computed");
+      return Math.random();
+    },
+  },
+  methods: {
+    methodsNumber: function () {
+      console.log("methods!");
+      return Math.random();
     },
   },
 });
